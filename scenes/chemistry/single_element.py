595ef47b88a5c6be5b8d08521b27e3a8cc52d1bb
Muetzilla <muetz-github@gasbichler.ch>
2025-02-08 15:34:26 +0100

    Multiple Elements

--- scenes/chemistry/single_element.py
+++ scenes/chemistry/single_element.py
@@ -10,7 +10,7 @@ class DrawSingleElement(Scene):
     def construct(self):
         self.add(
             MElementObject.from_csv_file_data(
-                filename=elements_filename, atomic_number=6
+                filename=elements_filename, atomic_number=26
             )
         )
 
@@ -22,3 +22,56 @@ class DrawMagnesium(Scene):
                 filename=elements_filename, atomic_number=12
             )
         )
+
+class DrawTwoElements(Scene):
+    def construct(self):
+        magnesium_element = MElementObject.from_csv_file_data(
+            filename=elements_filename, atomic_number=12
+        )
+        
+        oxygen_element = MElementObject.from_csv_file_data(
+            filename=elements_filename, atomic_number=8
+        )
+
+        magnesium_element.shift(LEFT * 2)
+        oxygen_element.shift(RIGHT * 2)
+
+        self.add(magnesium_element, oxygen_element)
+
+        self.play(
+            oxygen_element.animate.shift(UP * 2),
+            magnesium_element.animate.shift(UP * 2),
+            run_time=2
+        )
+
+
+        self.wait(2)
+
+class DrawMultipleElements(Scene):
+    def construct(self):
+        magnesium_element = MElementObject.from_csv_file_data(
+            filename=elements_filename, atomic_number=12
+        )
+        
+        oxygen_element = MElementObject.from_csv_file_data(
+            filename=elements_filename, atomic_number=8
+        )
+
+        iron_element = MElementObject.from_csv_file_data(
+            filename=elements_filename, atomic_number=26
+        )
+
+        magnesium_element.shift(LEFT * 2)
+        oxygen_element.shift(RIGHT * 2)
+
+        self.add(magnesium_element, oxygen_element,iron_element)
+
+        self.play(
+            oxygen_element.animate.shift(UP * 2),
+            magnesium_element.animate.shift(UP * 2),
+            run_time=2
+        )
+
+
+        self.wait(2)
+
