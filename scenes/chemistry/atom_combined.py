a56d6a867e3c505184dba729acbc5e6206b2e2e1
Muetzilla <muetz-github@gasbichler.ch>
2025-02-09 23:52:21 +0100

    Rendering Magnesium atom + informatin working (well text not that good :( )

--- scenes/chemistry/atom_combined.py
+++ scenes/chemistry/atom_combined.py
@@ -1,35 +1,42 @@
 from manim import *
 from manim_chemistry import *
 from pathlib import Path
+from manim.utils.tex_templates import TexTemplate
+
 
 script_path = Path(__file__).absolute().parent
 files_path = script_path / "element_files" 
 elements_filename = files_path / "Elements.csv"
 
 
+tex_template = TexTemplate()
+tex_template.add_to_preamble(r"\usepackage{mhchem}")
+
+
 class MagnesiumAtomAnimation(Scene):
     def construct(self):
-        title = Text("Magnesium Atom", font_size=32)
+        title = Tex(r"\ce{Magnesium Element}", tex_template=tex_template)
         title.to_edge(UP)
 
         atom_bohr = BohrAtom(e=12, p=12, n=14)
         atom_bohr.scale(0.5)
         atom_bohr.shift(3 * RIGHT)
+        atom_center = atom_bohr.get_center()
+
+        atom_information = MElementObject.from_csv_file_data(filename=elements_filename, atomic_number=12)
+        atom_information.shift(3 * LEFT).scale(0.5)
+        atom_information.set_stroke(width=0)
 
         self.play(Write(title))
-        self.add(
-            MElementObject.from_csv_file_data(
-                filename=elements_filename, atomic_number=12
-            ).shift(3 * LEFT)
-        )
+        self.add(atom_information)
+        self.add(atom_bohr)
         self.play(
             Rotate(
                 atom_bohr,
                 angle=2 * PI,
-                about_point=atom_bohr.get_center(),
+                about_point=atom_center,
                 rate_func=linear,
             ),
             run_time=3
         )
-
-        self.wait(0.5)
+        self.wait(1)
